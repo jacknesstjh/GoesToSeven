21e4f611c37eb424ffe600bbcd9bb187ab9c1b34
jacknesstang 唐俊辉 <jackness@126.com>
2017-09-11 20:26:13 +0800

    first commit

---
+++ test1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	x := []int{4:0}
+	fmt.Println(x)
+}
---
+++ test2/main.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	x := []int{}
+	fmt.Println(x)
+}
